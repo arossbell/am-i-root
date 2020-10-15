3345d6f9188851281227d8c7cf394693a7c7ccdf
Adam R Bell <a_0x07@protonmail.ch>
2020-10-14 23:00:27 -0600

    Thought of one more interesting scenario for this code.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // Spawn a root shell if a root setuid is on this binary.
 // Don't be stupid with this code.
 
-use users::{get_effective_uid, get_current_uid};
+use users::{get_effective_uid, get_current_uid, get_user_by_uid};
 use std::process::{Command, Stdio};
 use std::os::unix::process::CommandExt;
 
@@ -12,11 +12,14 @@ fn main() {
     let euid = get_effective_uid();
     if uid == 0 && euid == 0 {
         println!("Yes.");
-    } else if uid != 0 && euid != 0 {
+    } else if uid != 0 && euid != 0 && uid == euid {
         println!("No.");
     } else if uid != 0 && euid == 0 {
         println!("Now you are.");
-        Command::new("/bin/bash").uid(0).stdin(Stdio::inherit()).stdout(Stdio::inherit()).output();
+        Command::new("/bin/bash").uid(euid).stdin(Stdio::inherit()).stdout(Stdio::inherit()).output().unwrap();
+    } else if uid != 0 && euid != 0 && uid != euid {
+        println!("You aren't root, but now you're {}.", get_user_by_uid(euid).unwrap().name().to_string_lossy());
+        Command::new("/bin/bash").uid(euid).stdin(Stdio::inherit()).stdout(Stdio::inherit()).output().unwrap();
     } else {
         panic!("An unexpected UID/EUID combination happened!");
     }
